c35884df5ad2f747ea11bf979b092846875c8ba5
Shreyansh Jain <sh4yansh@gmail.com>
2019-01-17 09:55:25 +0530

    included subtraction module

---
+++ src/operations/calSub.java
@@ -0,0 +1,14 @@
+public class Subtract {
+	float a,b,result;
+	Subtract(float a, float b) {
+		this.a=a;
+		this.b=b;
+		sub();
+	}
+	void sub() {
+		this.result = this.a - this.b;
+	}
+	float getResult() {
+		return this.result;
+	}
+}
